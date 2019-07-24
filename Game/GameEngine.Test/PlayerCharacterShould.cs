779842a491132dff11f5943c344a1bacd9d0fefb
Sandip Punalkar <sandippunalkar9@gmail.com>
2019-07-24 19:26:32 +0530

    TestInitialize in playerCharacter class

--- Game/GameEngine.Test/PlayerCharacterShould.cs
+++ Game/GameEngine.Test/PlayerCharacterShould.cs
@@ -9,12 +9,18 @@ namespace GameEngine.Test
     [TestClass]
     public class PlayerCharacterShould
     {
+        private PlayerCharacter playerCharacter;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            playerCharacter = new PlayerCharacter {FirstName = "Sandip", LastName = "Patil"};
+        }
+
         [TestMethod]
         [TestCategory("Player Defaults")]
         public void BeInexperiencedWhenNew()
         {
-            var playerCharacter = new PlayerCharacter();
-
             Assert.IsTrue(playerCharacter.IsNoob);
         }
 
@@ -22,8 +28,6 @@ namespace GameEngine.Test
         [TestCategory("Player Defaults")]
         public void NotHaveNickNameByDefault()
         {
-            var playerCharacter = new PlayerCharacter();
-
             Assert.IsNull(playerCharacter.Nickname);
         }
 
@@ -31,7 +35,6 @@ namespace GameEngine.Test
         [TestCategory("Player Defaults")]
         public void StartWithDefaultHealth()
         {
-            var playerCharacter = new PlayerCharacter();
             Assert.AreEqual(100,playerCharacter.Health);
         }
 
@@ -77,8 +80,7 @@ namespace GameEngine.Test
         [DynamicData(nameof(ExternalHealthDamageTestData.TestData),typeof(ExternalHealthDamageTestData))]
         [TestCategory("Player Health")]
         public void TakeDamage(int damage,int expectedHealth)
-        {
-            var playerCharacter = new PlayerCharacter();
+        { 
             playerCharacter.TakeDamage(damage);
             Assert.AreEqual(expectedHealth,playerCharacter.Health);
         }
@@ -87,7 +89,6 @@ namespace GameEngine.Test
         [TestCategory("Player Health")]
         public void TakeDamage_NotEqual()
         {
-            var playerCharacter = new PlayerCharacter();
             playerCharacter.TakeDamage(1);
             Assert.AreNotEqual(100, playerCharacter.Health);
         }
@@ -95,7 +96,6 @@ namespace GameEngine.Test
         [TestCategory("Player Health")]
         public void IncreaseHealthAfterSleeping()
         {
-            var playerCharacter = new PlayerCharacter();
             playerCharacter.Sleep();
             Assert.IsTrue(playerCharacter.Health >= 101 && playerCharacter.Health <= 200);
         }
@@ -103,25 +103,16 @@ namespace GameEngine.Test
         [TestMethod]
         public void CalculateFullName()
         {
-            var playerCharacter = new PlayerCharacter();
-            playerCharacter.FirstName = "Sandip";
-            playerCharacter.LastName = "Patil";
-
             Assert.AreEqual("Sandip Patil", playerCharacter.FullName);
         }
         [TestMethod]
         public void CalculateFullNameUpperCase()
         {
-            var playerCharacter = new PlayerCharacter();
-            playerCharacter.FirstName = "Sandip";
-            playerCharacter.LastName = "Patil";
-
             Assert.AreEqual("SANDIP PATIL", playerCharacter.FullName, true);
         }
         [TestMethod]
         public void HaveFullNameStartingWithFirstName()
         {
-            var playerCharacter = new PlayerCharacter();
             playerCharacter.FirstName = "Sandip";
 
             //Assert.IsTrue(playerCharacter.FullName.StartsWith("Sandip"));
@@ -131,7 +122,6 @@ namespace GameEngine.Test
         [TestMethod]
         public void HaveFullNameStartingWithLastName()
         {
-            var playerCharacter = new PlayerCharacter();
             playerCharacter.LastName = "Patil";
 
             //Assert.IsTrue(playerCharacter.FullName.StartsWith("Sandip"));
@@ -141,20 +131,12 @@ namespace GameEngine.Test
         [TestMethod]
         public void CalculateFullName_SubstringAsserExample()
         {
-            var playerCharacter = new PlayerCharacter();
-            playerCharacter.FirstName = "Sandip";
-            playerCharacter.LastName = "Patil";
-
             StringAssert.Contains(playerCharacter.FullName,"ip Pa");
         }
 
         [TestMethod]
         public void CalculateFullNameWithTitleCase()
         {
-            var playerCharacter = new PlayerCharacter();
-            playerCharacter.FirstName = "Sandip";
-            playerCharacter.LastName = "Patil";
-
             StringAssert.Matches(playerCharacter.FullName, new Regex("[A-Z]{1}[a-z]+ [A-Z]{1}[a-z]+"));
             //StringAssert.DoesNotMatch(playerCharacter.FullName, new Regex("[A-Z]{1}[a-z]+ [A-Z]{1}[a-z]+"));
         }
@@ -162,21 +144,17 @@ namespace GameEngine.Test
         [TestMethod]
         public void HaveALongBow()
         {
-            var playerCharacter = new PlayerCharacter();
             CollectionAssert.Contains(playerCharacter.Weapons,"Long Bow");
         }
 
         [TestMethod]
         public void NotHaveAStaffOfWonder()
         {
-            var playerCharacter = new PlayerCharacter();
             CollectionAssert.DoesNotContain(playerCharacter.Weapons, "Staff Of Wonder");
         }
         [TestMethod]
         public void HaveAllExpectedWeapons()
         {
-            var playerCharacter = new PlayerCharacter();
-
             var expectedWeapoons = new[]
             {
                 "Long Bow",
@@ -189,8 +167,6 @@ namespace GameEngine.Test
         [TestMethod]
         public void HaveAllExpectedWeapons_AnyOrder()
         {
-            var playerCharacter = new PlayerCharacter();
-
             var expectedWeapoons = new[]
             {
                 "Short Bow",
@@ -203,8 +179,7 @@ namespace GameEngine.Test
         [TestMethod]
         public void HaveNoDuplicateWepons()
         {
-            var playerCharacter = new PlayerCharacter();
-           // playerCharacter.Weapons.Add("Short Bow");
+            // playerCharacter.Weapons.Add("Short Bow");
 
             CollectionAssert.AllItemsAreUnique(playerCharacter.Weapons);
         }
@@ -212,14 +187,12 @@ namespace GameEngine.Test
         [TestMethod]
         public void HaveAtLeastOneKindOfSword()
         {
-            var playerCharacter = new PlayerCharacter();
             Assert.IsTrue(playerCharacter.Weapons.Any(w => w.Contains("Sword")));
         }
 
         [TestMethod]
         public void HaveNoEmptyDefaultWepons()
         {
-            var playerCharacter = new PlayerCharacter();
             Assert.IsFalse(playerCharacter.Weapons.Any(w => string.IsNullOrWhiteSpace(w)));
         }
     }
